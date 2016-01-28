415dda09be202de4003a3b9f7183d445c256c081
Student <student@RU-102.local>
2016-01-28 11:07:21 -0700

    functions2

---
+++ functions2.js
@@ -0,0 +1,21 @@
+
+//------------------------------------
+
+var objName = {
+	name: 'Name',
+	age: '65',
+};
+function getName (obj) {
+	var Name = (obj.name);
+	return Name;
+};
+
+testIt = getName(objName);
+console.log(testIt);
+
+
+//------------------------------------
+
+
+
+
