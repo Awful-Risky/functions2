7d3f693b7037a6dfec499fe6f585312f212f39d5
Student <student@RU-102.local>
2016-01-28 11:41:45 -0700

    functions2

--- functions2.js
+++ functions2.js
@@ -1,20 +1,86 @@
 
 //------------------------------------
 
-var objName = {
-	name: 'Name',
-	age: '65',
-};
-function getName (obj) {
-	var Name = (obj.name);
-	return Name;
-};
+// var objName = {
+// 	name: 'Name',
+// 	age: '65',
+// };
+// function getName (obj) {
+// 	var Name = (obj.name);
+// 	return Name;
+// };
 
-testIt = getName(objName);
-console.log(testIt);
+// testIt = getName(objName);
+// console.log(testIt);
 
 
 //------------------------------------
+
+
+// var arrayOfStrings = ['this', 'is', 'a', 'string']
+
+// function totalLetters (array) {
+// 	letterCount = 0;
+// 	for (var i = 0; i<array.length; i+=1) {
+// 		letterCount += array[i].length;
+// 	};
+// 	return letterCount;
+// }
+//  testIt = totalLetters(arrayOfStrings);
+//  console.log(testIt);
+
+
+//------------------------------------
+
+// function keyValue (arg1, arg2) {
+// 	var obj = {arg1 : arg2};
+// 	return obj;
+// }
+
+// testIt = keyValue('agr1', 'arg2');
+// console.log(testIt);
+
+//------------------------------------
+
+// var arrayOfStrings = ['this', 'is', 'a', 'string']
+
+// function negativeIndex (array, negNumber) {
+// 	var negIndex = (array[array.length + negNumber]);
+// 	return negIndex;
+// }
+
+// testIt = negativeIndex(arrayOfStrings, -2);
+// console.log(testIt);
+
+//------------------------------------
+function 
+
+
+// console.log(testIt);
+
+//------------------------------------
+
+
+
+// console.log(testIt);
+
+//------------------------------------
+
+
+
+
+// console.log(testIt);
+
+//------------------------------------
+
+
+
+// console.log(testIt);
+
+//------------------------------------
+
+
+
 
 
 
